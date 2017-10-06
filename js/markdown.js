4ab24555d071338135f22be9d5b8d5f48dad9de7
Tiago Luna <tiagodeluna@hotmail.com>
2017-10-06 15:52:14 -0300

    Applying Javascript Best Practices

--- js/markdown.js
+++ js/markdown.js
@@ -1,123 +1,144 @@
-var REGEX_HEADERS = /^#+/,
-	REGEX_ORDERED_LIST = null,
-	REGEX_UNORDERED_LIST = null,
-	REGEX_ITALICS = null,
-	REGEX_BOLD = null,
-	REGEX_STRIKE = null;
+var LineType = {
+	HEADER: 1,
+	PARAGRAPH: 2,
+	UNORDERED_LIST: 3,
+	ORDERED_LIST: 4,
+	QUOTE: 5
+}
 
-var Line = function() {
+var MarkdownLine = {
+    openingTag: null,
+    content: null,
+    closingTag: null,
+    type: null,
 
+    toString: funtion() {
+    	return openingTag + content + closingTag;
+    }
 };
 
-function convertToHtml(text) {
-	var lines = text == undefined ? [] : text.split('\n');
-	var output = '';
-	var emptyLnCounter = 0;
-	
-	for (var i = 0; i < lines.length; i++) {
-		var line = lines[i];
-		var trimLine = line.trim();
+var convertToHtml = (function(text) {
+    var REGEX_HEADERS = /^#+/;
+    var REGEX_ORDERED_LIST = null;
+    var REGEX_UNORDERED_LIST = null;
+    var REGEX_ITALICS = null;
+    var REGEX_BOLD = null;
+    var REGEX_STRIKE = null;
 
-		if (trimLine.length == 0) {
-			/* JUMP LINE */
-			emptyLnCounter++;
-			if (emptyLnCounter > 1) {
-				output += '<br />';
-			}
-		} else {
-			emptyLnCounter = 0;
-			var starting, content, ending;
+    return function (text) {
+        var lines = text == null ? [] : text.split("\n");
+        var output = "";
+        var emptyLnCounter = 0;
+        var line;
+        var trimLine;
+        
+        var len = lines.length;
+        for (var i = 0; i < len; i += 1) {
+            line = lines[i];
+            trimLine = line.trim();
 
-			/* FIND HEADERS */
-			var occur = REGEX_HEADERS.exec(line);
-			if (occur != null && occur.length > 0) {
-				var level = occur[0].length >= 6 ? 6 : occur[0].length;
-				starting = '<h'+level+'>';
-				content = line.substr(level);
-				ending = '</h'+level+'>';
-			} else {
-				// TODO: Other rules!
-				starting = '<p>';
-				content = line;
-				ending = '</p>';
-			}
+            if (trimLine.length == 0) {
+                /* JUMP LINE */
+                emptyLnCounter += 1;
+                if (emptyLnCounter > 1) {
+                    output += "<br />";
+                }
+            } else {
+                emptyLnCounter = 0;
+                var starting, content, ending;
 
-			output += starting + content + ending;
-		}
-	}
-	
-	return output;
-}
+                /* FIND HEADERS */
+                var occur = REGEX_HEADERS.exec(line);
+                if (occur != null && occur.length > 0) {
+                    var level = occur[0].length >= 6 ? 6 : occur[0].length;
+                    starting = "<h"+level+">";
+                    content = line.substr(level);
+                    ending = "</h"+level+">";
+                } else {
+                    // TODO: Other rules!
+                    starting = "<p>";
+                    content = line;
+                    ending = "</p>";
+                }
+
+                output += starting + content + ending;
+            }
+        }
+        
+        return output;
+    }
+})();
+
 
 /*
 MARKDOWN ELEMENTS TO PARSE
 
 Headers:
-	# H1
-	## H2
-	### H3
-	#### H4
-	##### H5
-	###### H6
-		RegEx: /^#+/
-		Becames: <h1></h1>, <h2></h2>, <h3></h3>, <h4></h4>, <h5></h5>, <h6></h6>
+    # H1
+    ## H2
+    ### H3
+    #### H4
+    ##### H5
+    ###### H6
+        RegEx: /^#+/
+        Becames: <h1></h1>, <h2></h2>, <h3></h3>, <h4></h4>, <h5></h5>, <h6></h6>
 
 Emphasys:
-	*asterisks*
-	_underscores_
-		RegEx:
-		Becames: <em></em>
-	**asterisks**
-	__underscores__
-		Regex:
-		Becames: <strong></strong>
-		Obs: match combined emphasis. e.g: **asterisks and _underscores_**
-	~~Scratch this.~~
-		Regex:
-		Becames:
-			in CSS: .strike {text-decoration: line-through;}
-			in HTML: <span class="strike"></span>
+    *asterisks*
+    _underscores_
+        RegEx:
+        Becames: <em></em>
+    **asterisks**
+    __underscores__
+        Regex:
+        Becames: <strong></strong>
+        Obs: match combined emphasis. e.g: **asterisks and _underscores_**
+    ~~Scratch this.~~
+        Regex:
+        Becames:
+            in CSS: .strike {text-decoration: line-through;}
+            in HTML: <span class="strike"></span>
 
 Lists:
-	* Item
-	- Item
-	+ Item
-		RegEx: 
-		Becames: <ul><li></li></ul>
-		Obs: To create a sublist, the 'child' element must have 3 more spaces than its parent
-	1. Item
-		RegEx: 
-		Becames: <ol><li></li></ol>
-		Obs: The number sequence doesn't matter, but the first element defines the starting number.
+    * Item
+    - Item
+    + Item
+        RegEx: 
+        Becames: <ul><li></li></ul>
+        Obs: To create a sublist, the "child" element must have 3 more spaces than its parent
+    1. Item
+        RegEx: 
+        Becames: <ol><li></li></ol>
+        Obs: The number sequence doesn't matter, but the first element defines the starting number.
 
 Links:
-	[link](https:...)
-	[link]<https:...>
-		RegEx:
-		Becames: <a href="http://...">link</a>
+    [link](https:...)
+    [link]<https:...>
+        RegEx:
+        Becames: <a href="http://...">link</a>
 
 Links with reference:
-	[link]
-	AND
-	[link]: (https:...) OR [link]: <https:...>
-		RegEx:
-		Becames: <a href="http://...">link</a>
+    [link]
+    AND
+    [link]: (https:...) OR [link]: <https:...>
+        RegEx:
+        Becames: <a href="http://...">link</a>
 
 Html special characters:
-	<
-	>
-	/
-		RegEx:
-		Becames: 
+    <
+    >
+    /
+        RegEx:
+        Becames: 
 
 Comments:
-	[//]: # ()
+    [//]: # ()
 
 
 References:
-	https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-	https://www.w3schools.com/jsref/jsref_obj_regexp.asp
-	https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
-	https://guides.github.com/features/mastering-markdown/
-	https://dillinger.io/
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+    https://www.w3schools.com/jsref/jsref_obj_regexp.asp
+    https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
+    https://guides.github.com/features/mastering-markdown/
+    https://dillinger.io/
 */
