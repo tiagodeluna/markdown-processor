9f0f6e315804f810f8827e8576f20ab3b8620365
Tiago Luna <tiagodeluna@hotmail.com>
2017-10-15 21:36:03 -0300

    Bold and italic emphasys handling

--- js/markdown.js
+++ js/markdown.js
@@ -3,8 +3,14 @@ var convertToHtml = (function(text) {
     var REGEX_HEADERS = /^#{1,6}/;
     var REGEX_ORDERED_LIST = null;
     var REGEX_UNORDERED_LIST = null;
-    var REGEX_ITALICS = /_\B(.+?)\B_/;
-    var REGEX_BOLD = /__\B(.+?)\B__/;
+//    var REGEX_ITALIC_1 = /_\B(.+?)\B_/g;
+//    var REGEX_ITALIC_1 = /_(.*?)(?=_)_/g;
+    var REGEX_ITALIC_1 = /_(.+?)_/g;
+    var REGEX_ITALIC_2 = /\*(.+?)\*/g;
+//    var REGEX_BOLD_1 = /__(\S.+?\S)__/g;
+//    var REGEX_BOLD_1 = /__(.*?)(?=__)__/g;
+    var REGEX_BOLD_1 = /__(.+?)__/g;
+    var REGEX_BOLD_2 = /\*\*(.+?)\*\*/g;
     var REGEX_STRIKE = null;
 
     var LineType = {
@@ -13,9 +19,7 @@ var convertToHtml = (function(text) {
         PARAGRAPH: 3,
         UNORDERED_LIST: 4,
         ORDERED_LIST: 5,
-        QUOTE: 6,
-        ITALICS: 7,
-        BOLD: 8
+        QUOTE: 6
     };
 
     var ListPosition = {
@@ -70,14 +74,6 @@ var convertToHtml = (function(text) {
                     opening = "<quote>";
                     closing = "</quote>";
                     break;
-                case LineType.ITALICS:
-                    opening = "<em>";
-                    closing = "</em>";
-                    break;
-                case LineType.BOLD:
-                    opening = "<strong>";
-                    closing = "</strong>";
-                    break;
                 default:
                     break;
             }
@@ -85,19 +81,34 @@ var convertToHtml = (function(text) {
         },
 
         applyEmphasys: function(text) {
+
+            function boldReplacer(match, p1, offset, text) {
+                return ["<strong>", p1, "</strong>"].join("");
+            }
+
+            function italicReplacer(match, p1, offset, text) {
+                return ["<em>", p1, "</em>"].join("");
+            }
+
             if (text != null && text.length > 0) {
-                var occur = REGEX_BOLD.exec(text);
-                if (occur != null && occur.length > 0) {
+                /* REPLACE BOLD AND ITALIC OCCURRENCES */
+                return text.replace(REGEX_BOLD_1, boldReplacer)
+                        .replace(REGEX_BOLD_2, boldReplacer)
+                        .replace(REGEX_ITALIC_1, italicReplacer)
+                        .replace(REGEX_ITALIC_2, italicReplacer);
+                /*
+                if (b_occur != null && b_occur.length > 0) {
                     //Instantiate a new "BOLD" element
                     var element = Object.create(MarkdownElement);
                     element.type = LineType.BOLD;
-                    element.content = occur[1];
+                    element.content = b_occur[1];
                     //Get the text before and after the emphasys content
-                    var before = text.substr(0,occur.index);
-                    var after = text.substr(occur.index+occur[0].length);
+                    var before = text.substr(0,b_occur.index);
+                    var after = text.substr(b_occur.index+b_occur[0].length);
 
                     return before + element + this.applyEmphasys(after);
                 }
+                */
             }
 
             return text;
@@ -153,23 +164,17 @@ MARKDOWN ELEMENTS TO PARSE
 
 Headers:
     # (H1) to ###### (H6)
-        RegEx: /^#+/
         Becames: <h1></h1>, <h2></h2>, <h3></h3>, <h4></h4>, <h5></h5>, <h6></h6>
 
 Emphasys:
     *asterisks*
     _underscores_
-        RegEx:
         Becames: <em></em>
     **asterisks**
     __underscores__
-        Regex:
-        /
-        /__[.]+__/
         Becames: <strong></strong>
         Obs: match combined emphasis. e.g: **asterisks and _underscores_**
     ~~Scratch this.~~
-        Regex:
         Becames:
             in CSS: .strike {text-decoration: line-through;}
             in HTML: <span class="strike"></span>
@@ -178,32 +183,25 @@ Lists:
     * Item
     - Item
     + Item
-        RegEx: 
         Becames: <ul><li></li></ul>
         Obs: To create a sublist, the "child" element must have 3 more spaces than its parent
     1. Item
-        RegEx: 
         Becames: <ol><li></li></ol>
         Obs: The number sequence doesn't matter, but the first element defines the starting number.
 
 Links:
     [link](https:...)
     [link]<https:...>
-        RegEx:
         Becames: <a href="http://...">link</a>
 
 Links with reference:
     [link]
     AND
     [link]: (https:...) OR [link]: <https:...>
-        RegEx:
         Becames: <a href="http://...">link</a>
 
 Html special characters:
-    <
-    >
-    /
-        RegEx:
+    <, >, /
         Becames: 
 
 Comments:
@@ -211,9 +209,12 @@ Comments:
 
 
 References:
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-    https://www.w3schools.com/jsref/jsref_obj_regexp.asp
-    https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
-    https://guides.github.com/features/mastering-markdown/
-    https://dillinger.io/
+    REGEX:
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
+        https://www.w3schools.com/jsref/jsref_obj_regexp.asp
+    MARKDOWN:
+        https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
+        https://guides.github.com/features/mastering-markdown/
+        https://dillinger.io/
 */
